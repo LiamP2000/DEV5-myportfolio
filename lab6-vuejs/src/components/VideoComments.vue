8c6ab90c8489f4da9b073951e677b6b3947b6cb0
LiamP2000 <liampeeters@live.be>
2022-11-16 12:32:57 +0100

    feat: get data from api

--- lab6-vuejs/src/components/VideoComments.vue
+++ lab6-vuejs/src/components/VideoComments.vue
@@ -1,21 +1,36 @@
 <script setup>
 
-import { ref, reactive } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 
 
  let title = ref("");
- let comments = reactive([]);
+ //let comments = reactive([]);
+ let comment = ref('');
 
  const addComment = () => {
    comments.push(title.value);
    console.log("werkt et?");
  };
 
+ //fetch reactions 
+ onMounted(() => {
+  // fetch API
+  let api_url = "https://lab5-p379.onrender.com/api/v1/messages/"
+    fetch(api_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+            
+        })
+
 </script>
 
 <template>
   <div>
     <h1>Reactions</h1>
+
+    
 
     <ul>
       <li v-for="comment in comments" :key="comment">
