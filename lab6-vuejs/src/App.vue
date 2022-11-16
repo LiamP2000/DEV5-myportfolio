84582a6a0c8dbecf816459d276bf095fd23f13c3
LiamP2000 <liampeeters@live.be>
2022-11-16 12:15:43 +0100

    feat: startup files lab6

---
+++ lab6-vuejs/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  // import VideoPlayer.vue
+  import VideoPlayer from './components/VideoPlayer.vue'
+  // import VideoDetails.vue
+  import VideoDetails from './components/VideoDetails.vue'
+  // import VideoComments.vue
+  import VideoComments from './components/VideoComments.vue'
+
+</script>
+
+<template>
+  <div class="app">
+    <VideoPlayer />
+    <VideoDetails />
+    <VideoComments />
+
+  </div>
+</template>
+
+<style scoped>
+  .app {
+    display: grid;
+    grid-template-columns: 8fr 4fr;
+    grid-template-rows: 4fr 8fr;
+    grid-template-areas:
+      "video video"
+      "details comments";
+
+  }
+
+</style>
---
+++ lab6-vuejs/src/components/VideoComments.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+
+
+ let title = ref("");
+ let comments = reactive([]);
+
+ const addComment = () => {
+   comments.push(title.value);
+   console.log("werkt et?");
+ };
+
+</script>
+
+<template>
+  <div>
+    <h1>Reactions</h1>
+
+    <ul>
+      <li v-for="comment in comments" :key="comment">
+        {{ comment }}
+      </li>
+    </ul>
+
+    <input type="text" v-model="title">
+    <button @click="addComment">Add comment</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
